fb33f7c1aefc2dc12eec855f0bd5f470c2fe8d16
DavidDoherty72 <dave.w.doherty@gmail.com>
2022-10-05 01:32:48 -0400

    Coin system and Pause menu Added

---
+++ Meatball Run Game/Assets/Scripts/CollectingCoins.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectingCoins : MonoBehaviour
+{
+    public int coins;
+    public Text coinText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider Col)
+    {
+        if (Col.gameObject.tag == "Coins")
+        {
+            coins = coins + 1;
+            //Col.gameObject.SetActive(false);
+            Destroy(Col.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        coinText.text = coins.ToString("0");
+    }
+}
