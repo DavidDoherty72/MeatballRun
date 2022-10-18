e4b21a480663628a5fe6dab6325515c006ab2ead
DavidDoherty72 <dave.w.doherty@gmail.com>
2022-10-17 20:21:27 -0400

    Attempted look behind camera. Result -FAIL. Changed UI on Credits Scene

--- Meatball Run Game/Assets/Scripts/FollowPlayer.cs
+++ Meatball Run Game/Assets/Scripts/FollowPlayer.cs
@@ -4,20 +4,21 @@ public class FollowPlayer : MonoBehaviour
 {
     public Transform player;
     public Vector3 offset;
-  //  public Vector3 lookBehind;
 
-    // Start is called before the first frame update
+    
 
 
     // Update is called once per frame
-    void Update(){
+    void Update()
+    {
         transform.position = player.position + offset;
 
-      //  if (Input.GetKey("c")) //LOOK BEHIND
-       // {
-       //     transform.position = player.position + lookBehind;
-       // }
-        
-    }
 
+
+        if (Input.GetKey("c"))
+        {
+            transform.Rotate(0, 180, 0);
+        }
+    }
 }
+      
