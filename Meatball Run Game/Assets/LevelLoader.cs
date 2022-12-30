9903bd6d18b80e9af9e61d1c0734d213e78fede7
DavidDoherty72 <dave.w.doherty@gmail.com>
2022-12-30 00:38:14 -0500

    Title Screen Background and animations, Level 1 collider at end of mission, Level 3 put rats in throne room, Level 4 changed collision detection, Added tree section, Added more ground to launch site, Level 5 Added effects for wormhole

---
+++ Meatball Run Game/Assets/LevelLoader.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour
+{
+    
+    public GameObject loadingScreen;
+    public Slider slider;
+    public Text progressText;
+
+
+    public void LoadLevel (int sceneIndex)
+    {
+         StartCoroutine(LoadASynchronously(sceneIndex));
+    }
+
+    IEnumerator LoadASynchronously (int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+            progressText.text = progress * 100f + "%";
+
+            yield return null;
+        }
+    }
+}
